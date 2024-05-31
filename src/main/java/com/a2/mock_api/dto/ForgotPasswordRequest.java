a92ccd93caf07f21191607419a11dd7ef27788bf
Aradhya Nepal <aradhya.1221@gmail.com>
2024-05-31 09:53:42 +0545

    Working with forgot password practice

--- src/main/java/com/a2/mock_api/dto/ForgotPasswordRequest.java
+++ src/main/java/com/a2/mock_api/dto/ForgotPasswordRequest.java
@@ -7,14 +7,17 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ForgotPasswordRequest {
 
     @NotEmpty(message = ValidationConstants.emailRequired)
     @Email(message = ValidationConstants.emailNotValid)
     private String email;
+
 
 
 }
